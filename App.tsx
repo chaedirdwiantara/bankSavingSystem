2d8ccad89fb2ac4eac6ede1d3bb4ec1bbe42eff6
AndiChaedirDwiantara <chaedirdwiantara@gmail.com>
2025-11-27 14:50:08 +0700

    refactor: update RootNavigator import from default to named.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
-import RootNavigator from './src/navigation/RootNavigator';
+import { RootNavigator } from './src/navigation/RootNavigator';
 
 const App = () => {
   return (
